5cc49ee646a2287849a4be976e42e8e0ef8ddb86
smd995 <smd99540@gmail.com>
2025-03-28 19:45:08 +0900

    first commit

---
+++ src/app/components/organisms/header/index.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+interface Props {
+  className?: string;
+}
+
+export const Header = (props: Props) => {
+  return (
+    <div className={clsx(props.className)}>
+      <div className="flex items-center">
+        <Image
+          src="/logo/Size=Large.svg"
+          alt="Home"
+          width={151}
+          height={40}
+          priority
+          className="hidden md:block"
+        />
+
+        <Image
+          src="/logo/Size=Small.svg"
+          alt="Home"
+          width={71}
+          height={40}
+          priority
+          className="block md:hidden"
+        />
+      </div>
+    </div>
+  );
+};
